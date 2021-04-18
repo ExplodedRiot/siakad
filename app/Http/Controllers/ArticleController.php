ac74c7e442505df3f67b1af606750842f77479c9
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-18 12:04:51 +0700

    add

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -36,7 +36,17 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if ($request->file('image')){
+            $image_name = $request->file('image')->store('images', 'public');
+        }
+
+        Artice::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'featured_image' => $image_name
+        ]);
+        return red redirect()->route('articles.index')
+        ->with('success', 'Articles Succesfully Added');
     }
 
     /**
