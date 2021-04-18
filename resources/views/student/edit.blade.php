6657284796678407840707b3247eccaa766ef7c3
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-18 12:09:09 +0700

    change

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -1,53 +1,29 @@
-@extends('student.layout')
- 
-@section('content')
- 
-<div class="container mt-5"> 
- <div class="row justify-content-center align-items-center">
- <div class="card" style="width: 24rem;">
- <div class="card-header">
- Edit Student Data
- </div>
- <div class="card-body">
- @if ($errors->any())
- <div class="alert alert-danger">
- <strong>Whoops!</strong> There were some problems with your input.<br><br>
- <ul>
- @foreach ($errors->all() as $error)
- <li>{{ $error }}</li>
- @endforeach
- </ul>
- </div>
- @endif
- <form method="post" action="{{ route('student.update', $Student->nim) }}" id="myForm">
- @csrf
- @method('PUT')
- <div class="form-group">
- <label for="Nim">Nim</label> 
- <input type="text" name="Nim" class="form-control" id="Nim" value="{{ $Student->nim }}" ariadescribedby="Nim" > 
- </div>
- <div class="form-group">
- <label for="Name">Name</label> 
- <input type="text" name="Name" class="form-control" id="Name" value="{{ $Student-
->name }}" aria-describedby="Name" > 
- </div>
- <div class="form-group">
- <label for="Class">Class</label> 
-<select name="Class" class="form-control">
-@foreach($class as $kls)
-    <option value="{{$kls->id}}" {{ $mahasiswa->class_id == $kls->id ? 'selected' : ''}}>{{$kls->class_name}}</option>
-@endforeach    
-</select>
- </div>
- <div class="form-group">
- <label for="Major">Major</label> 
- <input type="Major" name="Major" class="form-control" id="Major" value="{{ $Student-
->major }}" aria-describedby="Major" > 
- </div>
- <button type="submit" class="btn btn-primary">Submit</button>
- </form>
- </div>
- </div>
- </div>
-</div>
-@endsection
+@extends('layouts.app') 
+@section('content') 
+<div class="container"> 
+ <form action="/articles/{{$article->id}}" method="post" 
+enctype="multipart/form-data"> 
+ @csrf 
+ <div class="form-group"> 
+ <label for="title">Title</label> 
+ <input type="text" class="form-control" 
+required="required" name="title" value="{{$article->title}}"></br> 
+ </div> 
+ <div class="form-group"> 
+ <label for="content">Content</label> 
+ <input type="text" class="form-control" 
+required="required" name="content" value="{{$article-
+>content}}"></br> 
+ </div> 
+ <div class="form-group"> 
+ <label for="image">Image</label> 
+ <input type="file" class="form-control" 
+required="required" name="image" value="{{$article-
+>featured_image}}"></br> 
+ <img width="150px" src="{{asset('storage/'.$article-
+>featured_image)}}"> 
+ </div> 
+ <button type="submit" class="btn btn-primary floatright">Update Data</button> 
+ </form> 
+</div> 
+@endsection 
