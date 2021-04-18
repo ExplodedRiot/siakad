66592a427807e6c2520cf06a174420570cdd9155
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-18 11:59:15 +0700

    change create blade

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -1,50 +1,21 @@
-@extends('student.layout')
- 
-@section('content')
- 
-<div class="container mt-5">
- 
- <div class="row justify-content-center align-items-center">
- <div class="card" style="width: 24rem;">
- <div class="card-header"> Add Student
- </div>
- <div class="card-body">
- @if ($errors->any())
- <div class="alert alert-danger">
- <strong>Whoops!</strong> There were some problems with your input.<br><br>
- <ul>
- @foreach ($errors->all() as $error)
- <li>{{ $error }}</li>
- @endforeach
- </ul>
- </div>
- @endif
- <form method="post" action="{{ route('student.store') }}" id="myForm">
- @csrf
- <div class="form-group">
- <label for="Nim">Nim</label> 
- <input type="text" name="Nim" class="form-control" id="Nim" aria-describedby="Nim" > 
- </div>
- <div class="form-group">
- <label for="Name">Name</label> 
- <input type="Name" name="Name" class="form-control" id="Name" ariadescribedby="Name" > 
- </div>
- <div class="form-group">
- <label for="Class">Class</label> 
- <select name="Class" class="form-control">
-    @foreach($class as $kls)
-    <option value="{{$kls->id}}">{{$kls->class_name}}</option>
-    @endforeach
-    </select>
-</div>
- <div class="form-group">
- <label for="Major">Major</label> 
- <input type="Major" name="Major" class="form-control" id="Major" ariadescribedby="Major" > 
- </div>
- <button type="submit" class="btn btn-primary">Submit</button>
- </form>
- </div>
- </div>
- </div>
- </div>
-@endsection
+@extends('layouts.app') 
+@section('content') 
+<div class="container"> 
+ <form action="/articles" method="post" enctype="multipart/formdata"> 
+ @csrf 
+ <div class="form-group"> 
+ <label for="title">Title: </label> 
+ <input type="text" class="form-control" 
+required="required" name="title"></br> 
+ <label for="content">Content: </label> 
+ <textarea type="text" class="form-control" 
+required="required" name="content"></textarea></br> 
+ <label for="image">Image: </label> 
+ <input type="file" class="form-control" 
+required="required" name="image"></br> 
+ <button type="submit" name="submit" class="btn btn-primary 
+float-right">Save</button> 
+ </div> 
+ </form> 
+</div> 
+@endsection 
