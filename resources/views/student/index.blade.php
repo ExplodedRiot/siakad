1d99444d08ed3f5e59ba608c06d088d727671b97
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-18 11:56:17 +0700

    add

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,5 +1,25 @@
 @extends('student.layout')
 @section('content')
+@if ($message = Session::get('success')) 
+ <div class="alert alert-success"> 
+ <p>{{ $message }}</p> 
+ </div> 
+ @endif 
+ <table class="table table-bordered" style="width:95%;margin: 0 
+auto;"> 
+ <tr> 
+ <th>Title</th> 
+ <th>Content</th> 
+ <th>Image</th> 
+ </tr> 
+ @foreach ($articles as $article) 
+ <tr> 
+ <td>{{ $article->title }}</td> 
+ <td>{{ $article->content }}</td> 
+ <td><img width="150px" 
+src="{{asset('storage/'.$article->featured_image)}}"></td>
+
+
  <div class="row">
  <div class="col-lg-12 margin-tb">
  <div class="pull-left mt-2">
