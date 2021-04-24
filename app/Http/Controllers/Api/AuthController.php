58de2003bb6bd6fc139a7de8a846fdbe9b5408da
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-24 18:22:58 +0700

    add

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -26,3 +26,21 @@ public function register(RegisterRequest $request)
         'user' > $user,
     ]);
 }
+
+public function login(LoginRequest $request)
+{
+    $validated = $request > validated();
+
+    if(!Auth::attempt($validated)) {
+        return $this > apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+    }
+
+    $user = User::where('email', $validated['email'])>first();
+    $token = $user > createToken('auth_token')>plainTextToken;
+
+    return $this>apiSuccess([
+        'token' > $token,
+        'token_type' > 'Bearer',
+        'user' > $user,
+    ]);
+}
