666f9b1479239649a2f1e1f5bd1c241b438f0fdd
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-24 19:45:14 +0700

    tes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,4 @@ Route::middleware(['auth::sanctum'])>group(function () {
 });
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+);
