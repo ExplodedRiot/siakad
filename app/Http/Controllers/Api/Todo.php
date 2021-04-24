97ed9aa6af966c98587650e57add99588c544182
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-24 20:30:27 +0700

    add

--- app/Http/Controllers/Api/Todo.php
+++ app/Http/Controllers/Api/Todo.php
@@ -46,3 +46,50 @@ public function rules()
         'done' -> 'nullable|boolean',
     ];
 }
+
+public function index(){
+    $user = auth()->user();
+    $todoos = Todo::with('user')
+    ->where('user_id', $user->id)
+    ->get();
+
+    return $this->apiSuccess($todos);
+}
+
+public function store(TodoRequest $request){
+    $request->validated();
+
+    $user = auth()->user();
+    $todo = new Todo($request -> all ());
+    $todo->user()->associate($user);
+    $todo->save();
+
+    return $this->apiSuccess($todo->load('user'));
+}
+
+public function show(Todo $todo)
+{
+    return $this->apiSuccess($todo->load('user'));
+}
+
+public function update(TodoRequest $request, Todo $todo)
+{
+    $request->validated();
+    $todo->todo = $request->todo;
+    $todo->label = $request->label;
+    $todo->done = $request->done;
+    $todo->save();
+    return $this->apiSuccess($todo->load('user'));
+}
+
+public function destroy(Todo $todo)
+{
+    if (auth()->user()->id = $todo->user_id){
+        $todo->delete;
+        return $this->apiSuccess($todo);
+    }
+    return $this -> apiError(
+        'Unauthorized',
+        Response::HTTP_UNAUTHORIZED
+    );
+}
