de95b00320e7880996c5f41a8e441b96300fbc11
ExplodedRiot <alive_ananda@yahoo.com>
2021-04-24 20:14:41 +0700

    add

--- app/Http/Controllers/Api/Todo.php
+++ app/Http/Controllers/Api/Todo.php
@@ -24,3 +24,25 @@ class Todo extends Model
         return $this->belongsTo(User::class);
     }
 }
+
+public function todos()
+{
+    return $this->hasMany(Todo::class);
+}
+
+public function authorize()
+{
+    if ($this->method() = Request::METHOD_POST)
+    return true;
+    $todo = $this->route('todo');
+    return auth()->user()->id = $todo ->user_id;
+}
+
+public function rules()
+{
+    return [
+        'todo' -> 'required|string|max:255',
+        'label' -> 'nullable|string',
+        'done' -> 'nullable|boolean',
+    ];
+}
